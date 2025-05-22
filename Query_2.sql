a40abde61a919f83626f2374ed562f22f9b18a06
KaterinaDraginova <k.draginova27@gmail.com>
2025-05-22 16:29:01 +0300

    Add files via upload

---
+++ Query_2.sql
@@ -0,0 +1,34 @@
+--#Write a query to show the Venue and Capacity columns from the Venue table and 
+--#add a calculated column called Venue_size which describes the venues according to the table below:
+
+
+SELECT 
+	v.Venue,
+	v.Capacity,
+	CASE
+		WHEN v.Capacity IS NULL THEN 'Unknown'
+		WHEN v.Capacity < 1000 THEN 'Intimate'
+		WHEN v.Capacity < 10000 THEN 'Small'
+		WHEN v.Capacity < 50000 THEN 'Medium'
+		WHEN v.Capacity < 100000 THEN 'Large'
+		ELSE 'Enormous'
+	END AS Venue_size
+FROM Venue v;
+
+--#Write another query to show the Venue and Opening_date columns from the Venue table.
+--#Add a calculated column called Venue_era which describes venues according to the table below:
+
+
+SELECT 
+	v.Venue,
+	v.Opening_date,
+	CASE
+		WHEN v.Opening_date IS NULL THEN 'Unknown'
+		WHEN v.Opening_date < '1801-01-01' THEN 'Ancient'
+		WHEN v.Opening_date < '1901-01-01' THEN '19th Century'
+		WHEN v.Opening_date < '2001-01-01' THEN '20th Century'
+		ELSE '21st Century'
+	END AS Venue_era
+FROM dbo.Venue v;
+
+
