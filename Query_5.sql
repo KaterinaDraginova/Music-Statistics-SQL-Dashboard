374d9d0a65ca13981f9bfeb754b0d5e6af4ef49f
KaterinaDraginova <k.draginova27@gmail.com>
2025-05-23 12:52:10 +0300

    Add files via upload

---
+++ Query_5.sql
@@ -0,0 +1,21 @@
+CREATE OR ALTER PROC dbo.usp_Tour_Totals
+AS
+BEGIN
+	SELECT 
+		a.Artist,
+		t.Tour_name,
+		SUM(s.Tickets_sold) AS Total_tickets_sold,
+		SUM(s.Revenue_$) AS Total_revenue,
+		AVG(s.Tickets_sold) AS Avg_tickets_sold,
+		AVG(s.Revenue_$) AS Avg_revenue
+	FROM dbo.Artist a
+	JOIN dbo.Tour t 
+		ON a.Artist_ID = t.Artist_ID
+	JOIN dbo.Show s 
+		ON t.Tour_ID = s.Tour_ID
+	GROUP BY 
+		a.Artist,
+		t.Tour_name
+	ORDER BY 
+		Avg_revenue DESC;
+END;
